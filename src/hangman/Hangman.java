4acb04bb193743b778ef4881d6e643d175a3def1
gohv <hristovsotirovgeorgi@gmail.com>
2016-05-25 15:40:52 +0300

    Main Method!

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -4,10 +4,12 @@ import game.Game;
 import player.Prompter;
 
 public class Hangman {
+
 	public static void main(String[] args) {
 
 		Game newGame = new Game("grumpy");
 		Prompter player = new Prompter(newGame);
 		player.play();
+		
 	}
 }
